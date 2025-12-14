0f1fa3e47dd17e6c8c8167f9d91593bdaf03e9ec
Henri Goosen <hgoosen@purdue.edu>
2025-12-14 21:24:09 +0000

    Added Project and Projects component, css styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,26 @@
+function Project({ title, desc } : { title: string, desc: string }) {
+  return (
+    <div className="bg-(--highlight)">
+      <h3>{title}</h3>
+      <p>{desc}</p>
+    </div>
+  )
+}
+
+function Projects() {
+  return (
+    <div>
+      <h2>Projects</h2>
+      <Project title={"Budget Tracker"} desc={"C-based program that uses CLI input to manage a budget."} />
+    </div>
+  )
+}
+
 export default function Home() {
   return (
     <div>
-      Hello World! -Henri G
-
+      <h1>Henri Goosen</h1>
+      <Projects />
     </div>
   );
 }
