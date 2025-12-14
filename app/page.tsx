d5a959e046d493ba0f2d4c186a780e941d7691b2
Henri Goosen <hgoosen@purdue.edu>
2025-12-14 23:35:03 +0000

    Added background image, more CSS styling

--- app/page.tsx
+++ app/page.tsx
@@ -18,11 +18,25 @@ function Projects() {
   )
 }
 
+function TitleCard() {
+  return (
+    <div className="relative bg-cover bg-fixed h-screen w-full  flex flex-col items-center justify-center bg-[linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.5)),url(./images/web-background.jpg)]">
+      <h1 className="font-title text-8xl! text-white text-center mb-6">Henri Goosen</h1>
+      <p className="absolute top-1/2 mt-20 w-lg p-5 rounded-lg text-center bg-background/80">
+        Hello, I&apos;m Henri, an aspiring software developer with leadership experience as a resident assistant and 
+        technical skills from projects and my involvement in Purdue&apos;s CubeSat lab. Adept in Java, C/C++, and Python.
+      </p>
+    </div>
+  )
+}
+
 export default function Home() {
   return (
     <div>
-      <h1 className="font-title text-6xl! text-center">Henri Goosen</h1>
-      <Projects />
+      <TitleCard />
+      <div className="p-5">
+        <Projects />
+      </div>
     </div>
   );
 }
