4e37f97a9da17d892b257163a1a9c09f3cf792c4
Henri Goosen <hgoosen@purdue.edu>
2025-12-15 01:56:47 +0000

    Added profile photo

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image'
+import HenriPhoto from './images/henri.png'
+
 function Project({ title, desc } : { title: string, desc: string }) {
   return (
     <div className="bg-(--highlight) rounded-md p-3 m-3 w-xs ">
@@ -20,8 +23,9 @@ function Projects() {
 
 function TitleCard() {
   return (
-    <div className="relative bg-cover bg-fixed h-screen w-full  flex flex-col items-center justify-center bg-[linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.5)),url(./images/web-background.jpg)]">
+    <div className="px-4 relative bg-cover bg-fixed h-screen w-full  flex flex-col items-center justify-center bg-[linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.5)),url(./images/web-background.jpg)]">
       <h1 className="font-title text-8xl! text-white text-center mb-6">Henri Goosen</h1>
+      <Image src={HenriPhoto} alt="" height={700} width={400} className='absolute top-1/6 ml-350 mt-2.5'/>
       <p className="absolute top-1/2 mt-20 w-lg p-5 rounded-lg text-center bg-background/80">
         Hello, I&apos;m Henri, an aspiring software developer with leadership experience as a resident assistant and 
         technical skills from projects and my involvement in Purdue&apos;s CubeSat lab. Adept in Java, C/C++, and Python.
